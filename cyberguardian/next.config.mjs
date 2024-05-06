73d7b6df785fe54c966051f7a33bea980dfc3401
PatF3lix <pat_pg@hotmail.fr>
2024-05-06 12:20:59 +0100

    basic nextjs project configuration with typescript

---
+++ cyberguardian/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  // Enables React strict mode, which performs additional checks and warnings
+  // for potential issues in your application.
+  reactStrictMode: true,
+};
+
+// Export the nextConfig object as the default export from this module.
+export default nextConfig;
