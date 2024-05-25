7a2154de466e7b14f16eb63faf37d345009709e9
PatF3lix <pat_pg@hotmail.fr>
2024-05-25 14:40:05 +0100

    corrected the width of the main content to adapt to every pages needs

--- cyberguardian/components/layout/Layout/Layout.tsx
+++ cyberguardian/components/layout/Layout/Layout.tsx
@@ -1,6 +1,5 @@
 import type { ReactNode } from "react";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
 
 type LayoutProps = {
@@ -12,7 +11,6 @@ export default function Layout({ children }: LayoutProps) {
     <div className={styles.rootContainer}>
       <Header />
       <main className={styles.containerMain}>{children}</main>
-      <Footer />
     </div>
   );
 }
