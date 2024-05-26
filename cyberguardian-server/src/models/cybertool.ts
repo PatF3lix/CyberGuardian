36fb2bb8175aaaf7b6567d5756e848d615361b72
PatF3lix <pat_pg@hotmail.fr>
2024-05-26 19:25:06 +0100

    refactored the cybertoolsModel to follow typescript best practices

--- cyberguardian-server/src/models/cybertool.ts
+++ cyberguardian-server/src/models/cybertool.ts
@@ -1,13 +1,50 @@
 import mongoose from "mongoose";
 
-const cyberToolSchema = new mongoose.Schema({
-  category: { type: String, required: true },
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  url: { type: String, required: true },
-  logo: { type: String },
-  image: { type: String },
-});
+type CybertoolAttrs = {
+  category: string;
+  name: string;
+  description: string;
+  url: string;
+  logo: string;
+  image: string;
+};
+
+interface CybertoolDoc extends mongoose.Document {
+  category: string;
+  name: string;
+  description: string;
+  url: string;
+  logo: string;
+  image: string;
+}
+
+interface CybertoolModel extends mongoose.Model<CybertoolDoc> {
+  build(attrs: CybertoolAttrs): CybertoolDoc;
+}
+
+const cyberToolSchema = new mongoose.Schema(
+  {
+    category: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    url: { type: String, required: true },
+    logo: { type: String },
+    image: { type: String },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+cyberToolSchema.statics.build = (attrs: CybertoolAttrs) => {
+  return new CyberTool(attrs);
+};
 
 const CyberTool = mongoose.model("CyberTool", cyberToolSchema);
 
