fb901c0c41d46d3aa85db72d33a805c7e8b35621
PatF3lix <pat_pg@hotmail.fr>
2024-05-27 13:28:48 +0100

    implemented custom error handling for authentication errors and database connection error installed helmet and cors packages for security purposes

--- cyberguardian-server/src/routes/userRouter.ts
+++ cyberguardian-server/src/routes/userRouter.ts
@@ -4,6 +4,9 @@ import { validateSignUp } from "../middlewares/validateSignUp";
 import { validateSignIn } from "../middlewares/validateSignIn";
 import { Password } from "../services/Password";
 import jwt from "jsonwebtoken";
+import { DatabaseConnectionError } from "../errors/databaseConnectionError";
+import mongoose from "mongoose";
+import { AuthenticationError } from "../errors/authenticationError";
 
 const router = express.Router();
 
@@ -24,42 +27,35 @@ router.post("/signUp", validateSignUp, async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
   try {
-    // Verify if the email is already used for another account
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res
-        .status(400)
-        .send({ errors: [{ msg: "Email is already in use" }] });
+      throw new AuthenticationError("Email is already in use");
     }
 
-    // Hash the password
     const hashedPassword = await Password.toHash(password);
 
-    // Create and save new user to db
     const user = new User({ username, email, password: hashedPassword });
     const newUser = await user.save();
 
-    // Generate JWT
     const userJwt = generateToken({ id: newUser.id, email: newUser.email });
 
-    // Send the token and the user information to the client
     res.status(201).send({ user: newUser, token: userJwt });
   } catch (err) {
-    res.status(500).send({ errors: [{ msg: "Server error" }] });
+    if (err instanceof mongoose.Error) {
+      throw new DatabaseConnectionError();
+    }
+    throw err;
   }
 });
 
 router.post("/signIn", validateSignIn, async (req: Request, res: Response) => {
-  //extract user information
   const { email, password } = req.body;
 
   try {
-    //Search for existing user with entered email
     const existingUser = await User.findOne({ email });
 
-    //return error if
     if (!existingUser) {
-      return res.status(400).send({ errors: [{ msg: "Invalid credentials" }] });
+      throw new AuthenticationError("Invalid credentials");
     }
 
     const isPasswordCorrect = await Password.compare(
@@ -67,21 +63,20 @@ router.post("/signIn", validateSignIn, async (req: Request, res: Response) => {
       existingUser.password
     );
     if (!isPasswordCorrect) {
-      return res.status(400).send({
-        errors: [{ msg: "Invalid credentials" }],
-      });
+      throw new AuthenticationError("Invalid credentials");
     }
 
-    // Generate JWT
     const userJwt = generateToken({
       id: existingUser.id,
       email: existingUser.email,
     });
 
-    // Send the token to the client
     res.status(200).send({ token: userJwt });
   } catch (err) {
-    res.status(500).send({ errors: [{ msg: "Server error" }] });
+    if (err instanceof mongoose.Error) {
+      throw new DatabaseConnectionError();
+    }
+    throw err;
   }
 });
 
--- cyberguardian-server/src/server.ts
+++ cyberguardian-server/src/server.ts
@@ -1,33 +1,49 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import { json } from "body-parser";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import { populateCyberTools } from "./services/populateCybertools";
-import { cybertoolsRouter } from "./routes/cybertoolsRouter";
 import { usersRouter } from "./routes/userRouter";
+import { cybertoolsRouter } from "./routes/cybertoolsRouter";
+import { errorHandler } from "./middlewares/errorHandling";
+import "express-async-errors"; // This package helps handle async errors
+import helmet from "helmet";
+// import cors from "cors";
 
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 4001;
+// const allowedOrigins = ["https://cyberguardian.patf3lix-portfolio.cc"];
 
-// Ensure express can parse JSON
-app.use(express.json());
+const app = express();
 
-// Use your routers
-app.use("/api/cybertools", cybertoolsRouter);
+app.use(json());
+app.use(helmet());
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (origin != null && allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
 app.use("/api/users", usersRouter);
+app.use("/api/cybertools", cybertoolsRouter);
+
+app.use(errorHandler);
 
 mongoose
   .connect(process.env.DATABASE_URI || "")
-  .then(async () => {
-    console.log("Connected to database");
-
-    await populateCyberTools();
-
+  .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
   })
-  .catch((err?: Error | null) => {
-    console.error("Could not connect to database:", err);
+  .catch((err) => {
+    console.error("Could not connect to MongoDB:", err);
   });
